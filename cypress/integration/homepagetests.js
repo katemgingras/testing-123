2184729aa3241a2c8c5cd7d5554287e313bfeb7f
Kate Gingras <katemgingras@gmail.com>
2021-07-30 20:57:20 -0400

    A few tests

--- cypress/integration/homepagetests.js
+++ cypress/integration/homepagetests.js
@@ -1,11 +1,16 @@
 /* Check that elements are loaded onto the page */
-beforeEach({
-    cy.visit('/index.html')
+
+beforeEach(() => {
+    cy.visit('index.html')
 })
 
 describe('Check page content exists and is correct', () => {
-    it('Checks the h1 element is about dogs', () => {
-        cy.get('h1').should('exist')
+    it('Checks the h1 element displays correctly', () => {
+        cy.get('h1').should('be.visible')
             .should('contain', 'Dogs')
+    })
+    it('Checks that there is paragraph text', () => {
+        cy.get('p').should('be.visible')
+            .should('contain', 'This project')
     })
 })
