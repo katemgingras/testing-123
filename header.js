d24bc31c846a442168ab165f440441b0da5a3072
Kate Gingras <kate.gingras@coxautoinc.com>
2021-08-01 13:06:59 -0400

    styling fixes and moving things around

--- header.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("header").innerHTML =
-'<h1><a href="index.html">Testing-123</a></h1>'
-;
-
-document.getElementById("nav1").innerHTML =
-'<a href="index.html">Home</a>' + ' ' +
-'<a href="dogs.html">Dog App</a>' + ' ' +
-'<a href="test.html">Test Tech</a>' + ' ' +
-'<a href="about.html">Resume</a>';
---
+++ site/footer.js
@@ -0,0 +1,11 @@
+document.getElementById("footer").innerHTML =
+    '<p id="footer">Site copyright Kate Gingras, 2021</p>'
+    ;
+
+document.getElementById("nav2").innerHTML =
+    '<p id="footer">' +
+    '<a href="index.html">Home</a>' + ' ' +
+    '<a href="dogs.html">Dog App</a>' + ' ' +
+    '<a href="test.html">Test Tech</a>' + ' ' +
+    '<a href="about.html">Resume</a>'
+    '</p>';
